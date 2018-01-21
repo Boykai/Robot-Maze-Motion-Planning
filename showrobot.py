fc4e718e060bc1fae5a9c0179f6ee18b5aff428a
Boy Kai <JToolesiii@gmail.com>
2018-01-21 14:29:39 -0500

    Feature: Add start_maze() to create baseline maze env

--- showrobot.py
+++ showrobot.py
@@ -27,3 +27,46 @@ class ShowRobot(object):
         self.env.hideturtle()
         self.env.penup()
         
+    def start_maze(self):
+        '''
+        Creates the baseline maze envirnment from the information within the
+        given .txt tester maze file.
+        
+        This env does not yet include the robot agent's path.
+        '''
+        # Iterate through squares one by one to decide where to draw walls.
+        for x in range(self.test_maze.dim):
+            for y in range(self.test_maze.dim):
+                if not self.test_maze.is_permissible([x,y], 'up'):
+                    self.env.goto(self.origin + self.sq_size * x,
+                                  self.origin + self.sq_size * (y+1))
+                    self.env.setheading(0)
+                    self.env.pendown()
+                    self.env.forward(self.sq_size)
+                    self.env.penup()
+    
+                if not self.testmaze.is_permissible([x,y], 'right'):
+                    self.env.goto(self.origin + self.sq_size * (x+1),
+                                  self.origin + self.sq_size * y)
+                    self.env.setheading(90)
+                    self.env.pendown()
+                    self.env.forward(self.sq_size)
+                    self.env.penup()
+    
+                # Only check bottom wall if on lowest row.
+                if y == 0 and not self.test_maze.is_permissible([x,y], 'down'):
+                    self.env.goto(self.origin + self.sq_size * x,
+                               self.origin)
+                    self.env.setheading(0)
+                    self.env.pendown()
+                    self.env.forward(self.sq_size)
+                    self.env.penup()
+    
+                # Only check left wall if on leftmost column.
+                if x == 0 and not self.test_maze.is_permissible([x,y], 'left'):
+                    self.env.goto(self.origin,
+                               self.origin + self.sq_size * y)
+                    self.env.setheading(90)
+                    self.env.pendown()
+                    self.env.forward(sq_size)
+                    self.env.penup()
