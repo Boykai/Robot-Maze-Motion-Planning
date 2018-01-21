f3f49286b22aae46a1d2abf6fddeb3e29fc8280f
Boy Kai <JToolesiii@gmail.com>
2018-01-21 14:09:40 -0500

    Chore: Create showrobot.py for showing robot exploring test mazes

---
+++ showrobot.py
@@ -0,0 +1,9 @@
+from maze import Maze
+import turtle
+import sys
+from robot import Robot
+
+class ShowRobot(object):
+    def __init__(self, test_maze):
+        self.test_maze = test_maze
+        
